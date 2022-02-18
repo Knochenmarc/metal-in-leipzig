e8a41c3a83719f75e94d47944ea62b2dfdd5ebe7
exochron <exochron@freenet.de>
2022-02-18 21:27:55 +0100

    - mehr im Auensee

--- src/Site/HausAuensee.php
+++ src/Site/HausAuensee.php
@@ -4,45 +4,56 @@ declare(strict_types=1);
 
 namespace MetalLE\Site;
 
+use Generator;
 use MetalLE\Event\Event;
 use MetalLE\Event\Location;
 use MetalLE\Site\Shop\Eventim;
+use MetalLE\Site\Shop\LiveGigs;
+use MetalLE\Site\Shop\ShopCrawler;
 use Traversable;
 
 class HausAuensee implements Site
 {
     private const URL = 'https://haus-auensee-leipzig.de';
 
+    public function __construct(private Crawler $http = new Crawler())
+    {
+    }
+
     public function getIterator(): Traversable
     {
-        $eventim  = new Eventim('haus-auensee-leipzig-7301');
         $location = new Location('ha', 'Haus Auensee', self::URL);
 
-        $http = new Crawler();
-        $plainHtml = $http->get(self::URL);
+        $plainHtml = $this->http->get(self::URL);
         if (preg_match('#<div class="md-col md-col-8">.*</a>\s+</div>#isU', $plainHtml, $matches)) {
             $plainHtml = $matches[0];
         }
 
         if (preg_match_all('#<a href="(.*)".*</a>#isU', $plainHtml, $matches, PREG_SET_ORDER)) {
-            foreach ($eventim->fetchDates() as $date) {
-                foreach ($matches as $key => $match) {
-                    if (str_contains($match[0], $date->format('>d.<'))
-                        && str_contains($match[0], $date->format('>m.y<'))
-                        && preg_match('#<h3.*>(.*)</h3>#iU', $match[0], $subMatches)
-                    ) {
-                        $imgUrl  = null;
-                        $url     = self::URL . '/' . $match[1];
-                        $subPage = $http->get($url);
-                        if (preg_match('#<img src="(.*)".*class="block col-12"#iU', $subPage, $imgMatch)) {
-                            $imgUrl = self::URL . $imgMatch[1];
-                        }
+            yield from $this->filter(new Eventim('haus-auensee-leipzig-7301'), $matches, $location);
+            yield from $this->filter(new LiveGigs('Haus Auensee'), $matches, $location);
+        }
+    }
 
-                        yield new Event(html_entity_decode($subMatches[1]), $date, $location, $url, $imgUrl);
-
-                        unset ($matches[$key]);
-                        break;
+    private function filter(ShopCrawler $shop, array &$matches, Location $location): Generator
+    {
+        foreach ($shop->fetchDates() as $date) {
+            foreach ($matches as $key => $match) {
+                if (str_contains($match[0], $date->format('>d.<'))
+                    && str_contains($match[0], $date->format('>m.y<'))
+                    && preg_match('#<h3.*>(.*)</h3>#iU', $match[0], $subMatches)
+                ) {
+                    $imgUrl  = null;
+                    $url     = self::URL . '/' . $match[1];
+                    $subPage = $this->http->get($url);
+                    if (preg_match('#<img src="(.*)".*class="block col-12"#iU', $subPage, $imgMatch)) {
+                        $imgUrl = self::URL . $imgMatch[1];
                     }
+
+                    yield new Event(html_entity_decode($subMatches[1]), $date, $location, $url, $imgUrl);
+
+                    unset ($matches[$key]);
+                    break;
                 }
             }
         }
--- src/Site/LinkedDataParser.php
+++ src/Site/LinkedDataParser.php
@@ -4,27 +4,21 @@ declare(strict_types=1);
 
 namespace MetalLE\Site;
 
-use MetalLE\Event\Event;
-use MetalLE\Event\Location;
-
 trait LinkedDataParser
 {
-    /**
-     * @return iterable|Event[]
-     * @throws \JsonException
-     */
-    private function iterateEvents(string $html, Location $location): iterable
+    private function iterateEvents(string $html): iterable
     {
         if (preg_match_all('#<script type="application/ld\+json">(.*)</script>#isU', $html, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $match) {
-                $event = json_decode($match[1], false, 512, JSON_THROW_ON_ERROR);
-                yield new Event(
-                    $event->name,
-                    new \DateTimeImmutable($event->startDate),
-                    $location,
-                    $event->url ?? $event->offers->url ?? $location->website,
-                    $event->image,
-                );
+                $events = json_decode($match[1], false, 512, JSON_THROW_ON_ERROR);
+                if (false === is_array($events)) {
+                    $events = [$events];
+                }
+                foreach ($events as $event) {
+                    if ($event->{"@type"} === 'Festival' || str_contains($event->{"@type"}, 'Event')) {
+                        yield $event;
+                    }
+                }
             }
         }
     }
