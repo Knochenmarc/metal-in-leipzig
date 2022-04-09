839cee685c3284dea986f13127d00846ec490cc9
exochron <exochron@freenet.de>
2022-04-09 11:01:04 +0200

    - updated täubchenthal

--- src/Site/Taeubchenthal.php
+++ src/Site/Taeubchenthal.php
@@ -13,21 +13,20 @@ use Traversable;
 
 class Taeubchenthal implements Site
 {
+    private const URL = 'https://www.taeubchenthal.com/';
+
     public function __construct(
-        private Location $location = new Location('tt', 'Täubchenthal', 'https://www.taeubchenthal.com/'),
+        private Location $location = new Location('tt', 'Täubchenthal', self::URL),
     ) {
     }
 
     public function getIterator(): Traversable
     {
         $http = new Crawler();
-        $json = $http->get(
-            'https://www.taeubchenthal.com/wp-admin/admin-ajax.php?id=&post_id=36&slug=programm&posts_per_page=100&page=0&offset=0&post_type=event&repeater=default&meta_key=evetndatum&meta_type=DATE&order=ASC&orderby=meta_value_num&action=alm_get_posts&query_type=standard'
-        );
-        $json = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
+        $html = $http->get(self::URL . 'programm');
         if (preg_match_all(
-            "#<div class='event'><a href='(.*)'.*url\((.*)\).*<span>(\d\d\.\d\d\.\d\d\d\d)</span><h3>(.*)</h3>#iU",
-            $json->html,
+            '#<div class="event event--list.*<img src="(.*)".*<h2><a href="(programm/.*)".*>(.*)</a></h2>.*<time datetime="(.*)">#isU',
+            $html,
             $matches,
             PREG_SET_ORDER
         )) {
@@ -40,13 +39,14 @@ class Taeubchenthal implements Site
     {
         foreach ($shop->fetchDates() as $date) {
             foreach ($matches as $key => $match) {
-                if ($date->format('d.m.Y') === $match[3]) {
+                $eventDate = new \DateTimeImmutable($match[4]);
+                if ($date->format('d.m.Y') === $eventDate->format('d.m.Y')) {
                     yield new Event(
-                        $match[4],
-                        $date,
+                        $match[3],
+                        $eventDate,
                         $this->location,
-                        $match[1],
-                        $match[2],
+                        self::URL . $match[2],
+                        self::URL . $match[1],
                     );
                     unset($matches[$key]);
                     continue 2;
