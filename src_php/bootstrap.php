c8d63ada0d80cf53cad7fb3b09895c1f16382f5c
exochron <exochron@freenet.de>
2022-11-05 11:26:57 +0100

    - remove php

--- src_php/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-spl_autoload_register(static function ($className) {
-    include_once __DIR__ . '/' . str_replace(['MetalLE\\', '\\'], ['', '/'], $className) . '.php';
-});
