d4726eba284c5a25310b033073ab015688287a6d
exochron <exochron@freenet.de>
2023-02-12 12:35:56 +0100

    - updated and activated soltmann

--- src/site/soltmann.rs
+++ src/site/soltmann.rs
@@ -1,9 +1,8 @@
 use std::borrow::Borrow;
 
-use html_escape::decode_html_entities;
+use chrono::NaiveDateTime;
 
-use crate::site::parse_linked_data_events;
-use crate::tools::date::parse_iso_datetime;
+use crate::event::EventType;
 use crate::{Event, Http, Location, Site};
 
 pub(crate) struct Soltmann<'l> {
@@ -30,23 +29,67 @@ impl Site for Soltmann<'_> {
     fn fetch_events(&self, http: &Http) -> Vec<Event> {
         let mut result = Vec::new();
 
-        // use event api instead with tag-filter
-        // https://www.soltmann-bar.de/wp-json/tribe/events/v1/events?per_page=100
         // doc: https://www.soltmann-bar.de/wp-json/tribe/events/v1/doc
 
-        let html = http
-            .get("https://www.soltmann-bar.de/veranstaltungen/liste/")
-            .unwrap();
+        let search_tags = ["metal", "rock", "punk", "thrash"];
+        let mut collected_tags: Vec<i64> = vec![];
 
-        let events = parse_linked_data_events(html.as_str());
-        for data_event in events {
-            let evt = Event::new(
-                decode_html_entities(data_event["name"].as_str().unwrap()).replace("@Soltmann", ""),
-                parse_iso_datetime(data_event["startDate"].as_str().unwrap()).unwrap(),
+        for search_tag in search_tags {
+            let mut tags = http
+                .get_json(
+                    &("https://www.soltmann-bar.de/wp-json/tribe/events/v1/tags?per_page=50&search="
+                        .to_owned() + &search_tag),
+                )
+                .expect("soltmann: could not fetch tags")["tags"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|item| item["id"].as_i64().unwrap())
+                .collect::<Vec<i64>>()
+                .to_owned();
+            collected_tags.append(&mut tags);
+        }
+
+        let tags_param: Vec<String> = collected_tags
+            .iter()
+            .map(|id| "&tags[]=".to_owned() + id.to_string().as_str())
+            .collect();
+        let events = http
+            .get_json(
+                &("https://www.soltmann-bar.de/wp-json/tribe/events/v1/events?per_page=100"
+                    .to_owned()
+                    + tags_param.join("").as_str()),
+            )
+            .expect("soltmann: could not fetch events");
+
+        for event in events["events"].as_array().unwrap() {
+            let mut evt = Event::new(
+                event["title"].as_str().unwrap().to_string(),
+                NaiveDateTime::parse_from_str(
+                    event["start_date"].as_str().unwrap(),
+                    "%Y-%m-%d %H:%M:%S",
+                )
+                .unwrap(),
                 self.location.borrow(),
-                data_event["url"].as_str().unwrap().to_string(),
-                Some(data_event["image"].as_str().unwrap().to_string()),
+                event["url"].as_str().unwrap().to_string(),
+                Some(event["image"]["url"].as_str().unwrap().to_string()),
             );
+
+            evt.end_date = Some(
+                NaiveDateTime::parse_from_str(
+                    event["end_date"].as_str().unwrap(),
+                    "%Y-%m-%d %H:%M:%S",
+                )
+                .unwrap(),
+            );
+
+            evt.evt_type = match event["categories"][0]["slug"].as_str() {
+                Some("konzert") => EventType::Concert,
+                Some("party") => EventType::Party,
+                Some("stream") => EventType::Online,
+                Some("festival") => EventType::Festival,
+                _ => EventType::Unknown,
+            };
 
             result.push(evt);
         }
