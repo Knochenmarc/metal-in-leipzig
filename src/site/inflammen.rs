1f2f86aa615afb3de7aad658a355e706a4f76bdd
exochron <exochron@freenet.de>
2023-10-18 21:56:03 +0200

    - updated IFOA 2024

--- src/site/inflammen.rs
+++ src/site/inflammen.rs
@@ -31,15 +31,15 @@ impl Site for InFlammen<'_> {
         let mut evt = Event::new(
             "In Flammen Open Air".to_string(),
             NaiveDateTime::new(
-                NaiveDate::from_ymd_opt(2023, 7, 13).unwrap(),
+                NaiveDate::from_ymd_opt(2024, 7, 11).unwrap(),
                 NaiveTime::default(),
             ),
             self.location.borrow(),
             "https://www.in-flammen.com/".to_string(),
-            Some("https://image.jimcdn.com/app/cms/image/transf/none/path/sfa7e4f2e650d1c8b/backgroundarea/i06ee0d469e3774ec/version/1669229669/image.jpg".to_string()),
+            Some("https://image.jimcdn.com/app/cms/image/transf/none/path/sfa7e4f2e650d1c8b/backgroundarea/i6f52df9656bc8d1b/version/1693393571/image.jpg".to_string()),
         );
         evt.end_date = Some(NaiveDateTime::new(
-            NaiveDate::from_ymd_opt(2023, 7, 15).unwrap(),
+            NaiveDate::from_ymd_opt(2024, 7, 13).unwrap(),
             NaiveTime::from_hms_opt(23, 59, 00).unwrap(),
         ));
         evt.evt_type = EventType::Festival;
