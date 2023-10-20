de5b1ae361c05c269cc4fabf02f495bc2942585d
exochron <exochron@freenet.de>
2023-10-20 21:47:17 +0200

    - readded darkflower

--- src/site/darkflower.rs
+++ src/site/darkflower.rs
@@ -1,4 +1,5 @@
 use std::borrow::Borrow;
+use std::collections::HashMap;
 
 use html_escape::decode_html_entities;
 
@@ -30,13 +31,16 @@ impl Site for Darkflower<'_> {
 
     fn fetch_events(&self, http: &Http) -> Vec<Event> {
         let mut result = Vec::new();
-        let json = http
-            .get_json("https://darkflower.club/wp-json/wp/v2/pages/932")
-            .unwrap();
-        let html = json["content"]["rendered"].as_str().unwrap();
+        let mut payload = HashMap::new();
+        payload.insert("cals[evcal_calendar_707][sc][cal_id]", "");
+        let json = http.post_json(
+            "https://darkflower.club/?evo-ajax=eventon_init_load",
+            payload,
+        );
+        let html = json["cals"]["evcal_calendar_707"]["html"].as_str().unwrap();
         for data_event in parse_linked_data_events(html) {
             let name = data_event["name"].as_str().unwrap();
-            if !name.contains("Darkflower Electro Party") {
+            if !name.contains("Darkflower Electro Party") && !name.contains("Synth-Electro Party") {
                 let date = parse_iso_datetime(data_event["startDate"].as_str().unwrap());
                 match date {
                     Ok(start_date) => {
@@ -44,7 +48,7 @@ impl Site for Darkflower<'_> {
                             decode_html_entities(decode_html_entities(name).to_mut()).to_string(),
                             start_date,
                             self.location.borrow(),
-                            data_event["url"].as_str().unwrap().to_string(),
+                            "https://darkflower.club/veranstaltungen/".to_string(), //data_event["url"].as_str().unwrap().to_string(),
                             Option::Some(data_event["image"].as_str().unwrap().to_string()),
                         ));
                     }
