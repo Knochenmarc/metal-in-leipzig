dd7d5b8cb22ccdfba642c0d0939e4dc1721a5bd8
Knochenmarc <knochenmarc@geekmail.de>
2023-12-30 09:54:03 +0100

    - json-ld von MB

--- src/Site/Moritzbastei.php
+++ src/Site/Moritzbastei.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace MetalLE\Site;
 
-use MetalLE\Event\Event;
 use MetalLE\Event\Location;
 use Traversable;
 
 class Moritzbastei implements Site
 {
+    use LinkedDataParser;
+
     public function getIterator(): Traversable
     {
         $location = new Location(
@@ -20,24 +21,8 @@ class Moritzbastei implements Site
 
         $http      = new Crawler();
         $plainHTML = $http->get('https://www.moritzbastei.de/programm/tag/metal');
-        if (
-            preg_match_all(
-                '#<div class="infobox.*<img src="(.*)".*<span class="date-display-single">(\d\d\.\d\d.\d\d\d\d)</span> / <span class="date-display-single">(\d\d:\d\d)</span>.*<div class="infobox-details-title">(.*)</div>.*<a href="(.*)">Mehr Infos</a>#isU',
-                $plainHTML,
-                $matches,
-                PREG_SET_ORDER
-            ) > 0
-        ) {
-            foreach ($matches as $match) {
-                yield new Event(
-                    html_entity_decode(strip_tags($match[4])),
-                    new \DateTimeImmutable($match[2] . ' ' . $match[3]),
-                    $location,
-                    'https://www.moritzbastei.de' . $match[5],
-                    $match[1],
-                    $match[5],
-                );
-            }
-        }
+        $plainHTML = substr($plainHTML, strpos($plainHTML, 'id="desc-tag"'));
+
+        yield from $this->iterateEvents($plainHTML, $location);
     }
 }
