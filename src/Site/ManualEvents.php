a87ae23723e0780ba6edfd54b51db289d811d77d
Knochenmarc <knochenmarc@geekmail.de>
2023-12-30 09:54:04 +0100

    - remove Impericon

--- src/Site/ManualEvents.php
+++ src/Site/ManualEvents.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MetalLE\Site;
 
-use MetalLE\Event\Event;
 use MetalLE\Event\Location;
 use Traversable;
 
@@ -17,17 +16,19 @@ class ManualEvents implements Site
 
     public function getIterator(): Traversable
     {
-        yield new Event(
-            'Impericon Festival',
-            new \DateTimeImmutable('23.04.2022'),
-            $this->messe,
-            'https://www.impericon.com/de/festival',
-            'https://www.impericon.com/media/impericon/festivals/impericonfest_2022/20210827_imp_fest_22_vo1_microseite_bands_de.jpg',
-        );
+//        yield new Event(
+//            'Impericon Festival',
+//            new \DateTimeImmutable('23.04.2022'),
+//            $this->messe,
+//            'https://www.impericon.com/de/festival',
+//            'https://www.impericon.com/media/impericon/festivals/impericonfest_2022/20210827_imp_fest_22_vo1_microseite_bands_de.jpg',
+//        );
+        return new \ArrayIterator([]);
     }
 
     public function getLocations(): iterable
     {
-        yield $this->messe;
+//        yield $this->messe;
+        return [];
     }
 }
