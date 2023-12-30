d3d7104f0989b9b3a43387a2a74abbb9379e4da8
Knochenmarc <knochenmarc@geekmail.de>
2023-12-30 09:54:03 +0100

    - fix markdown links

--- src/View/recht.php
+++ src/View/recht.php
@@ -14,11 +14,11 @@
 <p>Diese Webseite dient ausschließlich meinen persönlichen Gebrauch.</p>
 
 <h1>Datenschutzerklärung</h1>
-<p>Diese Website wird auf Servern von
-    [GitHub Inc.](https://www.github.com/), 88 Colin P Kelly Jr St, San Francisco, CA 94107, USA gehostet. GitHub kann
-    dabei persönliche Informationen des Besuchers wie z.B. die IP-Adresse in Log-Dateien abspeichern.
-    siehe: [GitHub Privacy
-    Statement](https://docs.github.com/en/github/site-policy/github-privacy-statement#github-pages)<br/>
+<p>Diese Website wird auf Servern von <a href="https://www.github.com/" target="_blank">GitHub Inc.</a>,
+    88 Colin P Kelly Jr St, San Francisco, CA 94107, USA gehostet. GitHub kann dabei persönliche Informationen des
+    Besuchers wie z.B. die IP-Adresse in Log-Dateien abspeichern. siehe:
+    <a href="https://docs.github.com/en/github/site-policy/github-privacy-statement#github-pages" target="_blank">GitHub
+        Privacy Statement</a><br/>
     Als Betreiber habe ich auf diese Daten <u>keinen</u> Zugriff.</p>
 <p>Es werden <u>keine</u> Cookies, Werbe- oder Trackingskripte eingesetzt.</p>
 
