8ded89d42a68c58e5fb02fe19bf53759e96e838b
Knochenmarc <Knochenmarc@users.noreply.github.com>
2024-12-10 16:17:45 +0000

    Deploying to gh-pages from @ Knochenmarc/metal-in-leipzig@5b6592628ea82c997f82b6582d1c475d7676b001 🚀

--- js/filter.js
+++ js/filter.js
@@ -1,13 +1,18 @@
-Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/usr/local/lib/node_modules/minify/node_modules/@jridgewell/gen-mapping/dist/gen-mapping.mjs' imported from /usr/local/lib/node_modules/minify/node_modules/@jridgewell/source-map/dist/source-map.mjs
-    at new NodeError (node:internal/errors:405:5)
-    at finalizeResolution (node:internal/modules/esm/resolve:327:11)
-    at moduleResolve (node:internal/modules/esm/resolve:980:10)
-    at defaultResolve (node:internal/modules/esm/resolve:1206:11)
-    at ModuleLoader.defaultResolve (node:internal/modules/esm/loader:404:12)
-    at ModuleLoader.resolve (node:internal/modules/esm/loader:373:25)
-    at ModuleLoader.getModuleJob (node:internal/modules/esm/loader:250:38)
-    at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:76:39)
-    at link (node:internal/modules/esm/module_job:75:36) {
-  url: 'file:///usr/local/lib/node_modules/minify/node_modules/@jridgewell/gen-mapping/dist/gen-mapping.mjs',
-  code: 'ERR_MODULE_NOT_FOUND'
-}
+(function (doc) {
+    const storeFilters = () => {
+        let formData = new FormData(doc.getElementById('event-filter'))
+        let data = {};
+        formData.forEach((value, key) => (data[key] = value));
+        localStorage.setItem('filter', JSON.stringify(data));
+    };
+    const init = JSON.parse(localStorage.getItem('filter') || '{}');
+
+    doc.querySelectorAll('input[form="event-filter"]').forEach(input => {
+        if (input.getAttribute('type') === 'checkbox' && input.getAttribute('name') in init) {
+            input.checked = true
+        }
+
+        // need proper form change handler :-/
+        input.addEventListener('change', storeFilters);
+    });
+})(document)
