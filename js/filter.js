4528de2ad084be6c5a81334b1c129ab8b2ce70be
Knochenmarc <Knochenmarc@users.noreply.github.com>
2024-12-10 07:53:31 +0000

    Deploying to gh-pages from @ Knochenmarc/metal-in-leipzig@b30f87e01b3672245f1b661101d1867ddd7527a2 🚀

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,13 @@
-(function(a){var b=()=>{let A=new FormData(a.getElementById('event-filter'));let _={};for(const[B,C] of A.entries())_[B]=C;localStorage.setItem('filter',JSON.stringify(_))},c=JSON.parse(localStorage.getItem('filter')||'{}');for(const input of a.querySelectorAll('input[form="event-filter"]')){(input.getAttribute('type')==='checkbox'&&input.getAttribute('name') in c)&&(input.checked=!0);input.addEventListener('change',b)}})(document);
+Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/usr/local/lib/node_modules/minify/node_modules/@jridgewell/gen-mapping/dist/gen-mapping.mjs' imported from /usr/local/lib/node_modules/minify/node_modules/@jridgewell/source-map/dist/source-map.mjs
+    at new NodeError (node:internal/errors:405:5)
+    at finalizeResolution (node:internal/modules/esm/resolve:327:11)
+    at moduleResolve (node:internal/modules/esm/resolve:980:10)
+    at defaultResolve (node:internal/modules/esm/resolve:1206:11)
+    at ModuleLoader.defaultResolve (node:internal/modules/esm/loader:404:12)
+    at ModuleLoader.resolve (node:internal/modules/esm/loader:373:25)
+    at ModuleLoader.getModuleJob (node:internal/modules/esm/loader:250:38)
+    at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:76:39)
+    at link (node:internal/modules/esm/module_job:75:36) {
+  url: 'file:///usr/local/lib/node_modules/minify/node_modules/@jridgewell/gen-mapping/dist/gen-mapping.mjs',
+  code: 'ERR_MODULE_NOT_FOUND'
+}
