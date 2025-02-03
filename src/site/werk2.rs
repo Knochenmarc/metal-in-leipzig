5944dbb5b9251abcb595128933bb8a72a5d55d50
Knochenmarc <marc@gamerstate.de>
2025-02-03 17:22:50 +0100

    update werk2

--- src/site/werk2.rs
+++ src/site/werk2.rs
@@ -1,9 +1,8 @@
 use std::borrow::Borrow;
 
-use chrono::Datelike;
 use regex::Regex;
 
-use crate::tools::date::parse_german_date;
+use crate::tools::date::parse_short_date;
 use crate::{Event, Http, Location, Site};
 
 const URL: &str = "https://www.werk-2.de";
@@ -32,70 +31,59 @@ impl Site for Werk2<'_> {
     fn fetch_events(&self, http: &Http) -> Vec<Event> {
         let mut result = Vec::new();
 
-        let reg = Regex::new("(?is)<div class='monat'>(?P<month>.*?)</div>.*?<div class='tag'>(?P<day>\\d\\d)</div>.*?<p class='typen'>(?P<typen>.*?)</p>.*?<h2><a href='(?P<url>.*?)'>(?P<name>.*?)</a>.*?<img .*?src='(?P<img>.*?)'").unwrap();
+        let list_reg = Regex::new("(?is)<ul .*?vak_liste.*?>(.*?)</ul>").unwrap();
+        let reg = Regex::new("(?is)<img.*? src='(?P<img>.*?)'.?/>.*?<p class='typen'>(?P<typen>.*?)</p>.*?<h2><a href='(?P<url>.*?)'>(?P<name>.*?)</a>.*?<p>(?P<date>\\d\\d\\.\\d\\d\\.\\d\\d)").unwrap();
 
-        let mut has_december = false;
-        let this_year = chrono::Utc::now().year();
-        let next_year = this_year + 1;
-        let mut previous_date = None;
+        let html = http
+            .get("https://www.werk-2.de/programm/suche/?q=Metal")
+            .unwrap();
+        let list_html = list_reg.captures(&html).unwrap().get(1).unwrap().as_str();
 
-        let urls = [URL, "https://www.werk-2.de/programm/vorschau"];
-        for url in urls {
-            let html = http.get(url).unwrap();
-            let list_position = html.find("<ul class='vak_liste'>");
-            for captures in reg.captures_iter(&html[list_position.unwrap_or(0)..]) {
-                let month = captures.name("month").unwrap().as_str();
-                let day = captures.name("day").unwrap().as_str();
-                let url = captures.name("url").unwrap().as_str();
-                let name = captures.name("name").unwrap().as_str();
-                let img = captures.name("img").unwrap().as_str();
-                let typen = captures.name("typen").unwrap().as_str();
+        for captures in reg.captures_iter(list_html) {
+            let url = captures.name("url").unwrap().as_str();
+            let name = captures.name("name").unwrap().as_str();
+            let img = captures.name("img").unwrap().as_str();
+            let typen = captures.name("typen").unwrap().as_str();
+            let date = captures.name("date");
 
-                if !has_december && month == "Dezember" {
-                    has_december = true;
-                }
+            if date.is_some()
+                && typen.starts_with("Programm > ")
+                && typen.to_lowercase().contains("metal")
+            {
+                // http head image
+                let image = {
+                    let formats = ["_detail", "_403"];
+                    let urls = formats.map(|s| format!("{}{}", URL, img.replace("_liste_250", s)));
 
-                let year = if has_december && month != "Dezember" {
-                    next_year
-                } else {
-                    this_year
+                    let mut result = format!("{}{}", URL, img);
+                    for url in urls {
+                        if http.exists(url.as_str()) {
+                            result = url;
+                            break;
+                        }
+                    }
+
+                    Some(result)
                 };
 
-                let date = parse_german_date(format!("{}. {} {}", day, month, year).as_str())
-                    .and_hms_opt(0, 0, 0)
-                    .unwrap();
+                let date = date.unwrap().as_str();
+                let date = if date.len() == 8 {
+                    let mut longer_date = date[..6].to_owned();
+                    longer_date.push_str("20");
+                    longer_date.push_str(&date[6..]);
+                    parse_short_date(longer_date.as_str())
+                } else {
+                    parse_short_date(date)
+                };
 
-                if (previous_date.is_none() || previous_date.unwrap() < date)
-                    && typen.to_lowercase().contains("metal")
-                {
-                    // http head image
-
-                    let image = {
-                        let formats = ["_detail", "_258", "_229"];
-                        let urls = formats.map(|s| format!("{}{}", URL, img.replace("_liste", s)));
-
-                        let mut result = format!("{}{}", URL, img);
-                        for url in urls {
-                            if http.exists(url.as_str()) {
-                                result = url;
-                                break;
-                            }
-                        }
-
-                        Some(result)
-                    };
-
-                    let evt = Event::new(
-                        name.to_string(),
-                        date,
-                        self.location.borrow(),
-                        format!("{}{}", URL, url),
-                        image,
-                    );
-                    result.push(evt);
-
-                    previous_date = Some(date);
-                }
+                let evt = Event::new(
+                    name.to_string(),
+                    date,
+                    self.location.borrow(),
+                    format!("{}{}", URL, url),
+                    image,
+                );
+                result.push(evt);
             }
         }
 
