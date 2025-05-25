a4b7ec4db44977cf6fd3cf58680e3fa17cf90d7a
Knochenmarc <Knochenmarc@users.noreply.github.com>
2025-05-25 08:11:48 +0000

    Deploying to gh-pages from @ Knochenmarc/metal-in-leipzig@59849291f9c01806f272843605859c747db63f5c 🚀

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,18 @@
-(function (doc) { const storeFilters = () => { let formData = new FormData(doc.getElementById('event-filter')); let data = {}; formData.forEach((value, key) => (data[key] = value)); localStorage.setItem('filter', JSON.stringify(data)); }; const init = JSON.parse(localStorage.getItem('filter') || '{}'); doc.querySelectorAll('input[form="event-filter"]').forEach(input => { if (input.getAttribute('type') === 'checkbox' && input.getAttribute('name') in init) { input.checked = true } /* need proper form change handler :-/ */ input.addEventListener('change', storeFilters); }); })(document)
+(function (doc) {
+    const storeFilters = () => {
+        let formData = new FormData(doc.getElementById('event-filter'));
+        let data = {};
+        formData.forEach((value, key) => (data[key] = value));
+        localStorage.setItem('filter', JSON.stringify(data));
+    };
+    const init = JSON.parse(localStorage.getItem('filter') || '{}');
+
+    doc.querySelectorAll('input[form="event-filter"]').forEach(input => {
+        if (input.getAttribute('type') === 'checkbox' && input.getAttribute('name') in init) {
+            input.checked = true
+        }
+
+        /* need proper form change handler :-/ */
+        input.addEventListener('change', storeFilters);
+    });
+})(document)
