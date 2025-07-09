26c6c6b232d3e1a9a5c5136799af1a18f88aaccf
Knochenmarc <marc@gamerstate.de>
2025-07-09 16:10:54 +0200

    bc: separate dates of combined event

--- src/site/bandcommunity.rs
+++ src/site/bandcommunity.rs
@@ -1,11 +1,11 @@
 use std::borrow::Borrow;
 
-use html_escape::decode_html_entities;
-
 use crate::event::{Event, EventStatus, EventType, Location};
 use crate::site::{parse_linked_data_events, Site};
-use crate::tools::date::parse_iso_datetime;
+use crate::tools::date::{parse_iso_datetime, parse_short_date};
 use crate::tools::Http;
+use html_escape::decode_html_entities;
+use regex::Regex;
 
 pub struct Bandcommunity<'l> {
     location: Location<'l, 'l, 'l>,
@@ -37,7 +37,10 @@ impl Site for Bandcommunity<'_> {
             return result;
         }
 
+        let subdates_reg: Regex = Regex::new("(?si)📆 (\\d+.\\d+.) (.*?)<").unwrap();
+
         let html = response.unwrap();
+        let mut last_name = String::new();
 
         for data_event in parse_linked_data_events(html.as_str()) {
             let description = data_event["description"].as_str().unwrap();
@@ -46,28 +49,50 @@ impl Site for Bandcommunity<'_> {
                 && !name.contains("Hip Hop")
                 && !description.contains("Rapper")
                 && !description.contains("HipHop")
+                && !last_name.eq_ignore_ascii_case(name)
             {
-                let mut evt = Event::new(
-                    decode_html_entities(name).to_string(),
-                    parse_iso_datetime(data_event["startDate"].as_str().unwrap()).unwrap(),
-                    self.location.borrow(),
-                    data_event["url"].as_str().unwrap().to_string(),
-                    Some(data_event["image"].as_str().unwrap().to_string()),
-                );
-
-                let lower_name = name.to_lowercase();
-                evt.evt_type = if lower_name.contains("festival") || lower_name.contains("festevil")
-                {
-                    EventType::Festival
+                last_name = name.to_string();
+                if description.contains("Termine:") && description.contains("📆 ") {
+                    for captures in subdates_reg.captures_iter(description) {
+                        let name = captures.get(2).unwrap().as_str();
+                        let mut evt = Event::new(
+                            decode_html_entities(name).to_string(),
+                            parse_short_date(captures.get(1).unwrap().as_str()),
+                            self.location.borrow(),
+                            data_event["url"].as_str().unwrap().to_string(),
+                            Some(data_event["image"].as_str().unwrap().to_string()),
+                        );
+                        evt.evt_type = EventType::Concert;
+                        result.push(evt);
+                    }
                 } else {
-                    EventType::Concert
-                };
+                    let start_date =
+                        parse_iso_datetime(data_event["startDate"].as_str().unwrap()).unwrap();
+                    let end_date =
+                        Some(parse_iso_datetime(data_event["endDate"].as_str().unwrap()).unwrap());
 
-                evt.end_date =
-                    Some(parse_iso_datetime(data_event["endDate"].as_str().unwrap()).unwrap());
-                evt.status = EventStatus::from_schema(data_event["eventStatus"].as_str().unwrap());
+                    let mut evt = Event::new(
+                        decode_html_entities(name).to_string(),
+                        start_date,
+                        self.location.borrow(),
+                        data_event["url"].as_str().unwrap().to_string(),
+                        Some(data_event["image"].as_str().unwrap().to_string()),
+                    );
 
-                result.push(evt);
+                    let lower_name = name.to_lowercase();
+                    evt.evt_type =
+                        if lower_name.contains("festival") || lower_name.contains("festevil") {
+                            EventType::Festival
+                        } else {
+                            EventType::Concert
+                        };
+
+                    evt.end_date = end_date;
+                    evt.status =
+                        EventStatus::from_schema(data_event["eventStatus"].as_str().unwrap());
+
+                    result.push(evt);
+                }
             }
         }
 
