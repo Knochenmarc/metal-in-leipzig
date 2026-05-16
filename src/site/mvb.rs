1e7792a1007105837e4aa184c20a6db971f3874b
Knochenmarc <marc@gamerstate.de>
2026-05-16 10:20:57 +0200

    fix mvb

--- src/site/mvb.rs
+++ src/site/mvb.rs
@@ -1,8 +1,8 @@
 use crate::event::{Event, Location};
 use crate::site::Site;
-use crate::tools::date::{get_today, parse_iso_datetime};
+use crate::tools::date::parse_short_date;
 use crate::tools::Http;
-use chrono::Months;
+use serde_json::{Map, Value};
 use std::borrow::Borrow;
 
 pub struct MVB<'l> {
@@ -19,6 +19,19 @@ impl MVB<'_> {
             },
         }
     }
+
+    fn get_value(&self, fields: &Map<String, Value>, key: &str) -> String {
+        fields
+            .get(key)
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .get("stringValue")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string()
+    }
 }
 impl Site for MVB<'_> {
     fn get_location(&self) -> &Location {
@@ -28,74 +41,33 @@ impl Site for MVB<'_> {
     fn fetch_events(&self, http: &Http) -> Vec<Event> {
         let mut result = Vec::new();
 
-        let next_year = get_today().checked_add_months(Months::new(12)).unwrap();
-        let next_year = next_year.format("%Y-%m-%d").to_string();
-
-        // WordPress api of MyCalendar plugin
-        // https://wordpress.org/plugins/my-calendar/
-        let json = http
-            .get_json(
-                format!("https://mvb-leipzig.de/wp-json/my-calendar/v1/events?to={next_year}",)
-                    .as_str(),
-            )
-            .unwrap();
-        json.as_object()
+        let url = "https://firestore.googleapis.com/v1/projects/mvb-leipzig-303a7/databases/(default)/documents/events";
+        let data = http.get_json(url).unwrap();
+        data.as_object()
+            .unwrap()
+            .get("documents")
+            .unwrap()
+            .as_array()
             .unwrap()
             .iter()
-            .for_each(|(date_key, val)| {
-                val.as_array().unwrap().iter().for_each(|val| {
-                    let data = val.as_object().unwrap();
-
-                    let begin = data
-                        .get("occur_begin")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-                    let end = data.get("occur_end").unwrap().as_str().unwrap().to_string();
-                    let post_id = data
-                        .get("event_post")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-                    let title = data
-                        .get("event_title")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-                    let description = data
-                        .get("event_desc")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-                    let image_url = data
-                        .get("event_image")
-                        .unwrap()
-                        .as_str()
-                        .unwrap()
-                        .to_string();
-
-                    if begin.starts_with(date_key) && description.contains("Metal") {
-                        let image = if image_url.ends_with("leerlogo.png") {
-                            None
-                        } else {
-                            Some(image_url)
-                        };
-
-                        let mut evt = Event::new(
-                            title,
-                            parse_iso_datetime(begin.as_str()).unwrap(),
-                            self.location.borrow(),
-                            format!("{}?p={post_id}", self.location.website),
-                            image,
-                        );
-                        evt.end_date = Some(parse_iso_datetime(end.as_str()).unwrap());
-                        result.push(evt);
-                    }
-                })
+            .for_each(|e| {
+                let fields = e
+                    .as_object()
+                    .unwrap()
+                    .get("fields")
+                    .unwrap()
+                    .as_object()
+                    .unwrap();
+                let info = self.get_value(fields, "info");
+                if info.to_lowercase().contains("metal") {
+                    result.push(Event::new(
+                        self.get_value(fields, "title"),
+                        parse_short_date(self.get_value(fields, "date").as_str()),
+                        self.location.borrow(),
+                        "https://mvb-leipzig.de/".to_string(),
+                        Some(self.get_value(fields, "img")),
+                    ));
+                }
             });
 
         result
